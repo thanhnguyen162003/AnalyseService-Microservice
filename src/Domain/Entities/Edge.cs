16db6ab078fe764261fa7ef954dbad5c7dd21dbd
thanhnguyen <thanhnxse1692003@gmail.com>
2024-10-03 22:45:05 +0700

    add field

--- src/Domain/Entities/Edge.cs
+++ src/Domain/Entities/Edge.cs
@@ -19,6 +19,10 @@ public class Edge : BaseAuditableEntity
     public string? Source { get; set; }
 
     public string? Target { get; set; }
+
+    public string? SourceHandle { get; set; }
+    
+    public string? TargetHandle { get; set; }
     
     public string? Type { get; set; }
 
