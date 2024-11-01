941947cd18d047f0996b64c9f363efa366569f66
ThanhNguyen <thanhnxse1692003@gmail.com>
2024-11-01 23:58:46 +0700

    update redis config

--- src/Application/Services/Cache/Intefaces/IValidationDistributedCache.cs
+++
@@ -1,8 +0,0 @@
-// using Microsoft.Extensions.Caching.Distributed;
-//
-// namespace Application.Services.CacheService.Intefaces;
-//
-// public interface IValidationDistributedCache : IDistributedCache
-// {
-//     
-// }
