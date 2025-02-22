95cd5f077bce1962b7f3f181c4c224f784adb4ba
ThanhNguyen <thanhnxse1692003@gmail.com>
2025-02-22 12:07:06 +0700

    fix

--- src/Application/Outbox/OutboxBackgroundService.cs
+++ src/Application/Outbox/OutboxBackgroundService.cs
@@ -1,39 +1,39 @@
-namespace Orders.Api.Outbox;
+//namespace Orders.Api.Outbox;
 
-internal class OutboxBackgroundService(
-    IServiceScopeFactory serviceScopeFactory,
-    ILogger<OutboxBackgroundService> logger) : BackgroundService
-{ 
-    private const int OutboxProcessorFrequency = 7;
+//internal class OutboxBackgroundService(
+//    IServiceScopeFactory serviceScopeFactory,
+//    ILogger<OutboxBackgroundService> logger) : BackgroundService
+//{ 
+//    private const int OutboxProcessorFrequency = 7;
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        try
-        {
-            logger.LogInformation("Starting OutboxBackgroundService...");
+//    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+//    {
+//        try
+//        {
+//            logger.LogInformation("Starting OutboxBackgroundService...");
 
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                using var scope = serviceScopeFactory.CreateScope();
-                var outboxProcessor = scope.ServiceProvider.GetRequiredService<OutboxProcessor>();
+//            while (!stoppingToken.IsCancellationRequested)
+//            {
+//                using var scope = serviceScopeFactory.CreateScope();
+//                var outboxProcessor = scope.ServiceProvider.GetRequiredService<OutboxProcessor>();
 
-                await outboxProcessor.Execute(stoppingToken);
+//                await outboxProcessor.Execute(stoppingToken);
 
-                // Simulate running Outbox processing every N seconds
-                await Task.Delay(TimeSpan.FromSeconds(OutboxProcessorFrequency), stoppingToken);
-            }
-        }
-        catch (OperationCanceledException)
-        {
-            logger.LogInformation("OutboxBackgroundService cancelled.");
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "An error occurred in OutboxBackgroundService");
-        }
-        finally
-        {
-            logger.LogInformation("OutboxBackgroundService finished...");
-        }
-    }
-}
+//                // Simulate running Outbox processing every N seconds
+//                await Task.Delay(TimeSpan.FromSeconds(OutboxProcessorFrequency), stoppingToken);
+//            }
+//        }
+//        catch (OperationCanceledException)
+//        {
+//            logger.LogInformation("OutboxBackgroundService cancelled.");
+//        }
+//        catch (Exception ex)
+//        {
+//            logger.LogError(ex, "An error occurred in OutboxBackgroundService");
+//        }
+//        finally
+//        {
+//            logger.LogInformation("OutboxBackgroundService finished...");
+//        }
+//    }
+//}
